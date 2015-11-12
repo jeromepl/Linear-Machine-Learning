71c32a6228e0a517d92d564511e7f092bd6305e8
Elie H <eli.harfouche@gmail.com>
2015-11-11 22:16:41 -0500

    Implemented train, not debugged, not commented, might not work

--- LinearClassifier.java
+++ LinearClassifier.java
@@ -38,18 +38,78 @@ public class LinearClassifier implements Serializable {
 		return max;
 	}
 	
-	public void train(double[][] data) {
-		Matrix matrixToSolve = new Matrix(dimension, dimension);
+	public void train(double[][] data, String[] labels) {
+
+	Matrix matrixToSolve = new Matrix(dimension, dimension);
+
+	//LHS matrix, nxn used 10 times
+	double[][] tempMatrix = new double[dimension][dimension];
+	//RHS matrix
+	double[][] solveM = new double [dimension][1];
+
+	//fill this up
+	double[][] solution = new double[classes.length][dimensions];
+
+	for(int cl=0; cl<classes.length;cl++){
+		int sum =0; //used for both summing coefficients and summing constants
+		
+		//Compute the coefficients in each index, going by column.
+		//Add reasoning later
+		//column
+		for(int a=0; a<dimension;a++){
+			//rows
+			for(int i = 0; i<dimension;i++)
+			{
+				sum =0;
+				//bytes
+				for(int j=0; j<dimension;j++)
+				{
+					//Diagonal of the matrix, square of the entry
+					if(i==a)
+						sum+= 2*(data[j][a]*data[j][a]);
+					else
+						sum+= data[j][a];
+				}
+				tempMatrix[a][i] =sum;
+			}
+		}
+		 
+
+			//Compute constants
+			for(int r=0; r<dimension;r++)
+			{
+				sum =0;	
+				for(double x: data[r]){
+				if(labels[r].equals(classes[cl]))
+					sum+= x;
+				}
+			//Fill up the RHS matrix 
+			solveM[r][0] =sum;	
+			}
+
+
+		//Super ugly way to create a matrix for both the RHS and LHS, solve it, then extra the resulting nx1 matrix and put it in the solution
+		solution[cl] = (new Matrix(tempMatrix).solve(new Matrix(solveM))).getArray()[0];	
+		
+		}
+
+	
+
+	theta = new Matrix(solution);
+
 	}
 	
 	public Map<String, Float> classDistribution(double[] data) {
 		Map<String, Float> distribution = new HashMap<String, Float>();
 
 		Matrix dataM = new Matrix(new double[][]{data});
+		
 		//Multiply the 1xn data matrix by the nxm theta matrix to result in our result of 1x10 propabilities matrix
 		Matrix distributionM = dataM.arrayTimes(theta);
+		
 		//Transform back to matrix
 		double[] result = distributionM.getArray()[0];
+
 		//fill the hasmap with the keys and propabilities
 		for(int i=0;i<classes.length; i++)
 		{
